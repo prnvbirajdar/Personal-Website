03bf64735ecd642dd1799b543639a6dcd61fe424
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-09 17:36:18 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  const { SITE_NAME } = process.env
+
+  return (
+    <h1 data-testid="helloH1" className="text-xl text-indigo-900">
+      Hello from <u>{SITE_NAME}</u>.
+    </h1>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
