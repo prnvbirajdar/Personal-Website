53412c2856521afafb25e2d500713d80823a0b96
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-09 23:03:05 -0500

    blog and header responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,11 +23,19 @@ const Home: NextPage = () => {
           </div>
         </div>
       </header> */}
-      <header className="container w-full px-6 py-24 mx-auto md:w-3/4 lg:w-2/4">
+      <div className="text-black px-5">
+        <p className="px-5 rounded py-1 bg-gray-300 block sm:hidden">Mobile</p>
+        <p className="px-5 rounded py-1 bg-red-300 hidden sm:block md:hidden">Sm</p>
+        <p className="px-5 rounded py-1 bg-green-300 hidden sm:hidden md:block lg:hidden">Md</p>
+        <p className="px-5 rounded py-1 bg-blue-300 hidden sm:hidden md:hidden lg:block xl:hidden">Lg</p>
+        <p className="px-5 rounded py-1 bg-yellow-300 hidden sm:hidden md:hidden lg:hidden xl:block 2xl:hidden">xl</p>
+        <p className="px-5 rounded py-1 bg-purple-300 hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:block">2xl</p>
+      </div>
+      <header className=" w-11/12 sm:w-4/6 px-6 sm:px-0 py-24 mx-auto md:w-4/6 lg:w-2/4">
         <div className="flex flex-col space-y-12 divide-y divide-gray-200">
           <div>
-            <h1 className="mb-2 text-4xl font-extrabold leading-snug tracking-normal md:text-6xl">
-              Hello, I&apos;m Pranav👋
+            <h1 className="mb-2 text-4xl font-extrabold leading-snug  md:leading-tight tracking-normal sm:text-5xl md:text-6xl">
+              Hi, I&apos;m Pranav👋
             </h1>
             <p className="mt-6 text-base md:text-xl font-normal ">
               I am a software developer based in Ottawa, ON focused on building beautiful interfaces and accessible
@@ -35,7 +43,7 @@ const Home: NextPage = () => {
             </p>
             <button
               type="button"
-              className="tracking-wider font-medium text-sm ring-inset mt-7 p-3 px-6 rounded bg-gray-700 hover:bg-gray-800 transition"
+              className="tracking-wider font-bold text-sm ring-inset mt-7 p-3 px-6 rounded text-black bg-yellow-300 hover:bg-yellow-400 transition"
             >
               RESUME
             </button>
@@ -43,7 +51,7 @@ const Home: NextPage = () => {
         </div>
       </header>
 
-      <section className="container w-full px-4 py-24 mx-auto md:w-3/4 lg:w-2/4">
+      <section className="w-11/12 px-4 py-24 mx-auto md:w-3/4 lg:w-2/4">
         <div className="mb-12 text-left md:text-center">
           <h2 className="mb-2 text-3xl font-extrabold leading-tight ">Skcript Blog</h2>
           <p className="text-lg ">
