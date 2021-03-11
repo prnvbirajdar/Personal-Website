e4577e05257b4045a80733cfea5915e7070d757e
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-11 02:37:18 -0500

    main page done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
             </p>
             <button
               type="button"
-              className="font-semibold tracking-wider text-sm my-7 p-2.5 px-5 rounded ring-2 ring-gray-400 ring-opacity-50 bg-gray-900 hover:bg-opacity-25 shadow transition"
+              className="font-semibold tracking-wider text-sm my-7 p-2.5 px-5 rounded ring-2 ring-gray-400 ring-opacity-50 bg-gray-900 bg-opacity-50 hover:bg-opacity-100 shadow transition"
             >
               Resume
             </button>
@@ -83,8 +83,8 @@ const Home: NextPage = () => {
         <div className="text-left mt-16">
           <h2 className="text-3xl sm:text-4xl font-bold ">Recent Projects</h2>
         </div>
-        <div className="flex flex-col space-y-6 divide-y divide-gray-500 ring-1 ring-gray-700 rounded mt-8">
-          <div className="bg-gray-900 bg-opacity-25 hover:bg-black p-4">
+        <div className="flex flex-col space-y-6 divide-y divide-gray-500 border border-gray-600 hover:border-gray-400 transition rounded mt-8">
+          <div className=" p-4 rounded">
             <a href="#" className="flex">
               <div>IMAGE</div>
               <div className="ml-4">
@@ -103,8 +103,8 @@ const Home: NextPage = () => {
             </a>
           </div>
         </div>
-        <div className="flex flex-col space-y-6 divide-y divide-gray-500 ring-1 ring-gray-700 rounded mt-4">
-          <div className="bg-gray-900 bg-opacity-25 hover:bg-black p-4">
+        <div className="flex flex-col space-y-6 divide-y divide-gray-500 border border-gray-600 hover:border-gray-400 transition rounded mt-8">
+          <div className=" p-4 rounded">
             <a href="#" className="flex">
               <div>IMAGE</div>
               <div className="ml-4">
@@ -140,13 +140,13 @@ const Home: NextPage = () => {
               href="#"
             >
               Say Hello!
-              <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              {/* <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fillRule="evenodd"
                   d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
                   clipRule="evenodd"
                 />
-              </svg>
+              </svg> */}
             </a>
           </div>
         </div>
