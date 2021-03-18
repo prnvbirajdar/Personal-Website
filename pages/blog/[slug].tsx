c076a7376b90c747be5d37f465d0d80a1bdea1bd
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-18 04:33:16 -0400

    slug conditions

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -54,53 +54,59 @@ const BlogPage: NextPage<BlogData> = ({ source, blogData }) => {
   const blogText = hydrate(source)
   return (
     <>
-      <article
-        className="text-gray-300 sm:px-4 py-16 mx-auto max-w-7xl pt-16 md:pt-28"
-        itemID="#"
-        itemScope
-        itemType="http://schema.org/BlogPosting"
-      >
-        <div className="w-full mx-auto mb-8 text-left sm:w-11/12 md:w-3/4 lg:w-1/2">
-          <img
-            src={blogData.cover_image}
-            className="object-fit  h-auto md:object-cover w-full md:max-h-64 bg-center rounded-lg"
-            alt="Blog Cover"
-          />
-          <h1
-            className="px-4 sm:px-0 mt-6 mb-6 text-3xl font-bold leading-tight text-white md:text-4xl"
-            itemProp="headline"
-            title={blogData.title}
-          >
-            {blogData.title}
-          </h1>
+      {blogData && blogText && (
+        <article
+          className="text-gray-300 sm:px-4 py-16 mx-auto max-w-7xl pt-16 md:pt-28"
+          itemID="#"
+          itemScope
+          itemType="http://schema.org/BlogPosting"
+        >
+          <div className="w-full mx-auto mb-8 text-left sm:w-11/12 md:w-3/4 lg:w-1/2">
+            <img
+              src={blogData.cover_image}
+              className="object-fit  h-auto md:object-cover w-full md:max-h-64 bg-center rounded-lg"
+              alt="Blog Cover"
+            />
+            <h1
+              className="px-4 sm:px-0 mt-6 mb-6 text-3xl font-bold leading-tight text-white md:text-4xl"
+              itemProp="headline"
+              title={blogData.title}
+            >
+              {blogData.title}
+            </h1>
 
-          {/* <div className="flex mb-6 space-x-2 text-sm">
+            {/* <div className="flex mb-6 space-x-2 text-sm">
             {blogData.tag_list.map((tag) => (
               <p key={tag} className="text-gray-900 bg-gray-100 select-none px-1.5 py-0.75 rounded">
                 {tag}
               </p>
             ))}
           </div> */}
-          <div className="flex justify-between px-4 sm:px-0">
-            <div className="flex items-center ">
-              <div className="avatar ">
-                <img className="rounded-full w-14 h-14" src={blogData.user.profile_image_90} alt={blogData.user.name} />
+            <div className="flex justify-between px-4 sm:px-0">
+              <div className="flex items-center ">
+                <div className="avatar ">
+                  <img
+                    className="rounded-full w-14 h-14"
+                    src={blogData.user.profile_image_90}
+                    alt={blogData.user.name}
+                  />
+                </div>
+                <div className="ml-2">
+                  <p className=" font-semibold "> {blogData.user.name}</p>
+                  <p className="text-sm text-gray-400">{format(parseISO(blogData.published_at), 'MMMM dd, yyyy')}</p>
+                </div>
               </div>
-              <div className="ml-2">
-                <p className=" font-semibold "> {blogData.user.name}</p>
-                <p className="text-sm text-gray-400">{format(parseISO(blogData.published_at), 'MMMM dd, yyyy')}</p>
+              <div className="self-center">
+                <p className="text-sm flex justify-end text-gray-400">{blogData.public_reactions_count} 💖</p>
+                <p className="text-gray-400 flex justify-end text-sm">{blogData.page_views_count} views</p>
               </div>
-            </div>
-            <div className="self-center">
-              <p className="text-sm flex justify-end text-gray-400">{blogData.public_reactions_count} 💖</p>
-              <p className="text-gray-400 flex justify-end text-sm">{blogData.page_views_count} views</p>
             </div>
           </div>
-        </div>
-        <div className=" px-4 sm:px-0 text-gray-300 w-full mx-auto prose prose-sm md:prose 2xl:prose-lg  md:w-3/4 lg:w-1/2">
-          {blogText}
-        </div>
-      </article>
+          <div className=" px-4 sm:px-0 text-gray-300 w-full mx-auto prose prose-sm md:prose 2xl:prose-lg  md:w-3/4 lg:w-1/2">
+            {blogText}
+          </div>
+        </article>
+      )}
     </>
   )
 }
