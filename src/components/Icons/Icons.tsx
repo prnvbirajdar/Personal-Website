a5327f0a3b215fb8902bff2b0f121133afb550b0
prnvbirajdar <prnvbirajdar@gmail.com>
2021-03-20 05:55:27 -0400

    arrow icon

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -155,6 +155,23 @@ const OpenLink = () => {
   )
 }
 
+const ClickArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="self-center ml-1 h-4 w-4 "
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+}
+
 const OpenLink2 = () => {
   return (
     <a aria-label="Open Link" className="rounded  text-gray-300 hover:text-gray-100 ">
@@ -230,6 +247,6 @@ const ScrollUp = () => {
   )
 }
 
-export { Moon, Sun, Github, OpenLink, OpenLink2, ScrollUp, Dumbell, Covid }
+export { Moon, Sun, Github, OpenLink, OpenLink2, ScrollUp, Dumbell, Covid, ClickArrow }
 
 export default Icons
--- src/components/Landing/LatestArticles.tsx
+++ src/components/Landing/LatestArticles.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { parseISO, format } from 'date-fns'
 
 import { Props } from '../../containers/Interfaces/Interface'
+import { ClickArrow } from '../Icons/Icons'
 
 const LatestArticles: React.FC<Props> = (props) => {
   const { devData } = props
@@ -53,7 +54,7 @@ const LatestArticles: React.FC<Props> = (props) => {
 
                 <div className="text-sm md:text-base font-normal text-gray-300">
                   {description}
-                  <Link href={`/blog/${slug}`}>
+                  {/* <Link href={`/blog/${slug}`}>
                     <a className="cursor-pointer ml-2 text-blue-500 hover:text-blue-700 focus:text-blue-700 inline-flex items-center">
                       Read More
                       <svg
@@ -69,7 +70,7 @@ const LatestArticles: React.FC<Props> = (props) => {
                         <path d="M12 5l7 7-7 7" />
                       </svg>
                     </a>
-                  </Link>
+                  </Link> */}
                 </div>
               </div>
             </Link>
@@ -78,18 +79,7 @@ const LatestArticles: React.FC<Props> = (props) => {
       <Link href="/blog">
         <div className=" flex justify-center p-3 text-gray-300">
           <div className="text-center text-sm   md:text-base cursor-pointer">More Articles</div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="self-center ml-1 h-4 w-4 "
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <ClickArrow />
         </div>
       </Link>
     </section>
