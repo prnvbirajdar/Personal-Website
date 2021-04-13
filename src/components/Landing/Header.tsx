deff7ec72c20e9149d6c212baf50293c3f78f9ed
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-12 22:33:33 -0400

    button padding

--- src/components/Landing/Header.tsx
+++ src/components/Landing/Header.tsx
@@ -6,11 +6,14 @@ const Main = () => {
   return (
     <main className="flex flex-col space-y-12 divide-y divide-gray-200 mb-32 ">
       <div>
-        <h1 className="text-4xl  md:text-5xl lg:text-6xl font-extrabold leading-tight tracking-tight dark:text-gray-100  sm:leading-10   md:leading-14">
-          Hi, I&apos;m Pranav👋
+        <p className="mt-3 sm:w-9/12  text-indigo-900 dark:text-indigo-400 md:w-10/12 lg:w-full text-base md:text-lg font-bold ">
+          HEY THERE,
+        </p>
+        <h1 className="text-4xl md:mb-6  md:text-5xl lg:text-6xl font-extrabold leading-tight tracking-tight dark:text-gray-100  sm:leading-10   md:leading-14">
+          I&apos;m Pranav👋
         </h1>
-        <p className="mt-3  mb-6 md:mb-7 sm:w-9/12  text-gray-900 dark:text-gray-300 md:w-10/12 lg:w-full text-base md:text-lg font-medium ">
-          I am a software developer building beautiful interfaces and accessible applications.
+        <p className="mt-3 mb-6 md:mb-8 sm:w-9/12  text-gray-900 dark:text-gray-300 md:w-10/12 lg:w-full text-base md:text-lg font-medium ">
+          I&apos;m a software developer building beautiful interfaces and accessible applications.
         </p>
 
         {/* <button
@@ -22,7 +25,7 @@ const Main = () => {
 
         <a
           href="mailto:prnvbirajdar@gmail.com"
-          className=" bg-indigo-800 bg-opacity-90 hover:bg-indigo-900 focus:bg-indigo-900 text-indigo-100 mt-6 text-sm font-bold lg:text-base tracking-normal my-5 md:my-7 p-2.5 px-4 md:px-5 rounded ring-2 ring-gray-400 ring-opacity-50 shadow "
+          className=" bg-indigo-800 bg-opacity-90 hover:bg-indigo-900 focus:bg-indigo-900 text-indigo-100 mt-6 text-sm font-bold lg:text-base tracking-normal my-5 md:my-7 p-3 px-4 md:px-5 rounded ring-2 ring-gray-400 ring-opacity-50 shadow "
         >
           Say Hello!
         </a>
