b630767c579091d006b365cb7f728fee9cc5f164
Pranav Birajdar <prnvbirajdar@gmail.com>
2022-01-31 00:34:43 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="max-w-5xl flex flex-col sm:flex-row  m-auto sm:justify-between items-center w-11/12">
       <Icons />
       <p className="text-sm mb-6 sm:mb-4 text-gray-800 dark:text-gray-400 sm:self-center">
-        Pranav Birajdar &copy; 2021
+        Pranav Birajdar &copy; {new Date().getFullYear()}
       </p>
     </footer>
   )
