5100f81f710c8d22186c617d425751c861f2db10
Pranav Birajdar <prnvbirajdar@gmail.com>
2023-07-07 03:26:46 +0000

    Updated link href

--- src/components/Landing/Header.tsx
+++ src/components/Landing/Header.tsx
@@ -31,7 +31,7 @@ const Main = () => {
             variants={fadeInUp}
             target="_blank"
             rel="noreferrer"
-            href="Pranav Birajdar's Resume.pdf"
+            href="Pranav's Resume.pdf"
             className=" bg-indigo-800 bg-opacity-90 hover:bg-indigo-900 focus:bg-indigo-900 text-indigo-100 mt-6 text-sm font-bold lg:text-base my-5 md:my-7 p-3 px-4 md:px-5 rounded ring-2 ring-gray-400 ring-opacity-50 shadow "
           >
             Resume
